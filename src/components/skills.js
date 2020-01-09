cc5c464083331c79259894ea3d5bb14afac63b6d
Danny Gleason <danny.c.gleason@gmail.com>
2020-01-09 13:31:47 -0500

    added names to skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -15,24 +15,31 @@ class Skills extends Component {
           <div className="skills">
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faHtml5} size='10x'/>
+                <h6>HTML5</h6>
               </div>
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faCss3} size='10x'/>
+               <h6>CSS3</h6>
               </div>
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faJs} size='10x'/>
+            <h6>Javascript</h6>
               </div>
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faBootstrap} size='10x'/>
+            <h6>Bootstrap</h6>
               </div>
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faReact} size='10x'/>
+            <h6>ReactJS</h6>
               </div>
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faGit} size='10x'/>
+            <h6>Git</h6>
               </div>
               <div className='item'>
             <FontAwesomeIcon icon={Icons.faGithub} size='10x'/>
+            <h6>Github</h6>
             </div>
           </div>        
         </div>
